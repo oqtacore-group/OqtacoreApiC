b859a59e575d5f4cda2a30447c3408f2f16fd78c
Mihaylin <info@flymining.cloud>
2020-11-23 03:05:44 -0500

    Oqtacore Api

---
+++ Controllers/MainController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OqtacoreMLM.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class MainController : ControllerBase
+    {
+     
+
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public class SendEbookModel
+        { 
+            public string name { get; set; }
+            public string email { get; set; }
+            public string post_id { get; set; }
+        }
+
+        public class SubscribeNewsletterModel
+        {
+            public string url { get; set; }
+            public string email { get; set; }
+        }
+
+        public class SubscribeNewsModel
+        {
+            public string email { get; set; }
+            public string post_id { get; set; }
+        }
+
+
+        [HttpPost]
+        [Route("/send_ebook")]
+        public bool Send_ebook(SendEbookModel model)
+        {
+
+            return true;
+        }
+
+        [HttpPost]
+        [Route("/subscribe_newsletter")]
+        public bool Subscribe_newsletter(SubscribeNewsletterModel model)
+        {
+
+            return true;
+        }
+
+        [HttpPost]
+        [Route("/subscribe_news")]
+        public bool Subscribe_news(SubscribeNewsModel model)
+        {
+
+            return true;
+        }
+    }
+}
